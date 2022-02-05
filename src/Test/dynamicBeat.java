071ad50830dfb6d5dfb261b0dc459fa5e547f588
SangYoo1324 <samuel13241@gmail.com>
2022-02-04 21:19:45 -0800

    gazuaaa

--- src/Test/dynamicBeat.java
+++ src/Test/dynamicBeat.java
@@ -18,7 +18,7 @@ public class dynamicBeat extends JFrame {
 	
 	private Image background = new ImageIcon(Main.class.getResource("/Images/mainBackground.jpg")).getImage(); //draw with g.drawImage
 	
-	
+	private Music introMusic = new Music("IntroMusic.mp3", true);
 	
 	//track components
 	private Image titleImage ;
@@ -51,6 +51,15 @@ public class dynamicBeat extends JFrame {
 	private JButton rightButton = new JButton(rightButtonBasicImage);
 	
 	
+	//game play Screen
+	private ImageIcon backButtonEnteredImage = new ImageIcon(Main.class.getResource("/Images/ArrowLeft.png"));
+	private ImageIcon backButtonBasicImage = new ImageIcon(Main.class.getResource("/Images/ArrowLeft.png"));
+	private JButton backButton = new JButton(backButtonBasicImage);
+	private Image gameInfoImage = new ImageIcon(Main.class.getResource("/Images/gameInfo.png")).getImage(); //draw with g.drawImage
+	private Image judgementLineImage = new ImageIcon(Main.class.getResource("/Images/judgementLine.png")).getImage(); //draw with g.drawImage
+	private Image noteRouteImage =  new ImageIcon(Main.class.getResource("/Images/noteRoute.png")).getImage();
+	private Image noteRouteLineImage =  new ImageIcon(Main.class.getResource("/Images/noteRouteLine.png")).getImage();
+	private Image noteBasicImage =  new ImageIcon(Main.class.getResource("/Images/noteBasic.png")).getImage();
 	
 	//easy hard buttons
 	private ImageIcon easyButtonEnteredImage = new ImageIcon(Main.class.getResource("/Images/easyButtonEntered.png"));
@@ -64,6 +73,8 @@ public class dynamicBeat extends JFrame {
 	
 	
 	private boolean isMainScreen =false;
+	private boolean isGameScreen = false;
+	
 	
 	ArrayList<Track> trackList = new ArrayList<Track>();
 	
@@ -84,7 +95,7 @@ dynamicBeat(){
 	trackList.add(new Track("farout_truestLiesText.png", "Truest_Lie.png", "mainPage.jpg", "Far Out - Truest Lies.mp3", "Far Out - Truest Lies.mp3"));
 	trackList.add(new Track("FelixCartal_LoveMeText.png", "Love me.png", "mainPage.jpg", "Felix Cartal  LIGHTS - Love Me.mp3", "Felix Cartal  LIGHTS - Love Me.mp3"));
 	
-	Music introMusic = new Music("IntroMusic.mp3", true);
+	
 	introMusic.start();
 	
 	exitButton.setBounds(1240 ,0,50, 50);
@@ -138,18 +149,10 @@ dynamicBeat(){
 		public void mousePressed(MouseEvent e) {
 			Music buttonPressedMusic = new Music("buttonPressedMusic.mp3",false);
 			buttonPressedMusic.start();
-			introMusic.close();
 			
+			enterMain();
 	//game start event
-			startButton.setVisible(false);
-			quitButton.setVisible(false);
-			leftButton.setVisible(true);
-			rightButton.setVisible(true);
-			hardButton.setVisible(true);
-			easyButton.setVisible(true);
-			background = new ImageIcon(Main.class.getResource("/Images/mainPage.jpg")).getImage();
-			isMainScreen =true;
-			selectedTrack(0);
+
 		}
 	});
 	add(startButton);
@@ -294,6 +297,33 @@ dynamicBeat(){
 	});
 	add(hardButton);
 	
+	backButton.setVisible(false);
+	backButton.setBounds(20 ,50,60, 60);
+	backButton.setBorderPainted(false);
+	backButton.setContentAreaFilled(false);
+	backButton.setFocusPainted(false);
+	backButton.addMouseListener(new MouseAdapter() {
+		@Override
+		public void mouseEntered(MouseEvent e) {
+			backButton.setIcon(backButtonEnteredImage);
+			Music buttonEnteredMusic = new Music("buttonPressedMusic.mp3",false);
+			buttonEnteredMusic.start();
+			backButton.setCursor(new Cursor(Cursor.HAND_CURSOR));
+		}
+		@Override
+		public void mouseExited(MouseEvent e) {
+			backButton.setIcon(backButtonBasicImage);
+		}
+		@Override
+		public void mousePressed(MouseEvent e) {
+			Music buttonPressedMusic = new Music("buttonPressedMusic.mp3",false);
+			buttonPressedMusic.start();
+		//main screen return event
+			backMain();
+		}
+	});
+	add(backButton);
+	
 	menuBar.setBounds(0,0, 1280,30);
 	menuBar.addMouseListener(new MouseAdapter() {
 		@Override
@@ -318,18 +348,62 @@ dynamicBeat(){
 
 }
 
-public void paint(Graphics g) {
+public void paint(Graphics g) {//Override AWT
 screenImage = createImage(Main.SCREEN_WIDTH, Main.SCREEN_HEIGHT);
 screenGraphic = screenImage.getGraphics();
-screenDraw(screenGraphic);
+screenDraw((Graphics2D)screenGraphic);// screenDraw to ScreenGraphic
 g.drawImage(screenImage,0,0,null);
 }
 
-public void screenDraw(Graphics g) {
+public void screenDraw(Graphics2D g) {
 	g.drawImage(background, 0,0,null);
 	if(isMainScreen) {
 		g.drawImage(selectedImage, 340,100,null);
 		g.drawImage(titleImage,340,70,null );
+	}
+	if(isGameScreen) {
+		g.drawImage(noteRouteImage, 228,30,null);
+		g.drawImage(noteRouteImage, 332,30,null);
+		g.drawImage(noteRouteImage, 436,30,null);
+		g.drawImage(noteRouteImage, 540,30,null);
+		g.drawImage(noteRouteImage, 640,30,null);
+		g.drawImage(noteRouteImage, 744,30,null);
+		g.drawImage(noteRouteImage, 848,30,null);
+		g.drawImage(noteRouteImage, 952,30,null);
+		
+		
+		g.drawImage(noteRouteLineImage, 224,30,null);
+		g.drawImage(noteRouteLineImage, 328,30,null);
+		g.drawImage(noteRouteLineImage, 432,30,null);
+		g.drawImage(noteRouteLineImage, 536,30,null);
+		g.drawImage(noteRouteLineImage, 740,30,null);
+		g.drawImage(noteRouteLineImage, 844,30,null);
+		g.drawImage(noteRouteLineImage, 948,30,null);
+		g.drawImage(noteRouteLineImage, 1052,30,null);
+		
+		g.drawImage(noteBasicImage, 540,500,null);
+		g.drawImage(noteBasicImage, 640,500,null);
+		
+		g.drawImage(gameInfoImage, 0,660,null);
+		g.drawImage(judgementLineImage, 0,580,null); 
+		
+		//g.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		g.setColor(Color.white);
+		g.setFont(new Font("Arial", Font.BOLD,30));
+		g.drawString("shibalsaeki", 20, 702);
+		
+		g.setFont(new Font("Elephant", Font.BOLD,30));
+		g.drawString("Easy", 1190, 700);
+		g.drawString("000000", 565, 702);
+		
+		g.setColor(Color.DARK_GRAY);
+		g.drawString("S", 270, 609);
+		g.drawString("D", 374, 609);
+		g.drawString("F", 478, 609);
+		g.drawString("SpaceBar", 580, 609);
+		g.drawString("J", 784, 609);
+		g.drawString("K", 889, 609);
+		g.drawString("L", 993, 609);
 	}
 	paintComponents(g);
 	this.repaint();
@@ -366,12 +440,37 @@ public void gameStart(int nowSelected, String difficulty) {
 	if(selectedMusic != null)
 		selectedMusic.close();
 	
+	isGameScreen = true;
 	isMainScreen = false;
 	leftButton.setVisible(false);
 	rightButton.setVisible(false);
 	easyButton.setVisible(false);
 	hardButton.setVisible(false);
+	backButton.setVisible(true);
 	background = new ImageIcon(Main.class.getResource("/Images/"+trackList.get(nowSelected).getGameImage())).getImage();
 	
+}
+public void backMain() {
+	isMainScreen =true;
+	leftButton.setVisible(true);
+	rightButton.setVisible(true);
+	easyButton.setVisible(true);
+	hardButton.setVisible(true);
+	backButton.setVisible(false);
+	background = new ImageIcon(Main.class.getResource("/Images/mainBackground.jpg")).getImage();
+	selectedTrack(nowSelected);
+	isGameScreen = false;
+}
+public void enterMain() {
+	startButton.setVisible(false);
+	quitButton.setVisible(false);
+	leftButton.setVisible(true);
+	rightButton.setVisible(true);
+	hardButton.setVisible(true);
+	easyButton.setVisible(true);
+	background = new ImageIcon(Main.class.getResource("/Images/mainPage.jpg")).getImage();
+	isMainScreen =true;
+	introMusic.close();
+	selectedTrack(0);
 }
 }
